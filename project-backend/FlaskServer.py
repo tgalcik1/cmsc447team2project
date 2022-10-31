c9194ed9e8691ff775bde7794eee05f701952f2e
cluke1 <cluke1@umbc.edu>
2022-10-31 16:23:44 -0400

    Added crime csv parsing function

--- project-backend/FlaskServer.py
+++ project-backend/FlaskServer.py
@@ -1,6 +1,16 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS, cross_origin
 from flask_mysqldb import MySQL
+import pandas as pd
+import mysql.connector
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  password="",
+  database="databaseName"
+)
+mycursor = mydb.cursor()
 
 app = Flask(__name__)
 cors = CORS(app)
@@ -30,6 +40,26 @@ def parseCSV_covidcases(filePath):
              mydb.commit()
              print(i,row['OBJECTID'],row['DATE'],row['Baltimore'],row['Baltimore_CITY'])
 
+def parseCSV_crime(filePath):
+    # CVS Column Names
+      col_names = ['X','Y','RowID', 'CrimeCode', 'Location', 'Description', 'Inside_Outside', 'Weapon',
+                    'Post', 'Gender', 'Age', 'Race', 'Ethnicity', 'District', 'Neighborhood', 'Latitude',
+                    'Longitude', 'GeoLocation', 'Premise', 'VRIName', 'Total_Incidents']
+      # Use Pandas to parse the CSV file
+      csvData = pd.read_csv(filePath,names=col_names, header=None)
+      # Loop through the Rows
+      for i,row in csvData.iterrows():
+             sql = "INSERT INTO addresses (X, Y, CrimeCode, Location, Description, Inside_Outside, Weapon,\
+                    Post, Gender, Age, Race, Ethnicity, District, Neighborhood, Latitude, Longitude, GeoLocation,\
+                    Premise, VRIName, Total_Incidents) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+             value = (row['X'],row['Y'],row['CrimeCode'],row['Location'], row['Description'],row['Inside_Outside'],row['Weapon'],row['Post'],
+                    row['Gender'],row['Age'],row['Race'],row['Ethnicity'], row['District'],row['Neighborhood'],row['Latitude'],row['Longitude'],
+                    row['GeoLocation'],row['Premise'],row['VRIName'],row['Total_Incidents'])
+             mycursor.execute(sql, value, if_exists='append')
+             mydb.commit()
+             print(i,row['X'],row['Y'],row['CrimeCode'],row['Location'], row['Description'],row['Inside_Outside'],row['Weapon'],row['Post'],
+                    row['Gender'],row['Age'],row['Race'],row['Ethnicity'], row['District'],row['Neighborhood'],row['Latitude'],row['Longitude'],
+                    row['GeoLocation'],row['Premise'],row['VRIName'],row['Total_Incidents'])
 
 if __name__=="__main__":
     main()
