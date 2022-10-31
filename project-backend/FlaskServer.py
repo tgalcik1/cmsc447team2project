79a66a2cfee86d2b89502e87166e6b18b6b21101
cluke1 <cluke1@umbc.edu>
2022-10-31 18:09:12 -0400

    Code added to upload crime data to SQL database

--- project-backend/FlaskServer.py
+++ project-backend/FlaskServer.py
@@ -1,31 +1,41 @@
-from flask import Flask, request, jsonify
+from flask import Flask, render_template, request, jsonify, redirect, url_for
 from flask_cors import CORS, cross_origin
 from flask_mysqldb import MySQL
 import pandas as pd
 import mysql.connector
+import os
+from os.path import join, dirname, realpath
 
 mydb = mysql.connector.connect(
   host="localhost",
   user="root",
   password="",
-  database="databaseName"
+  database="cmsc447project"
 )
 mycursor = mydb.cursor()
 
 app = Flask(__name__)
 cors = CORS(app)
 
+UPLOAD_FOLDER = 'static/files'
+app.config['UPLOAD_FOLDER'] =  UPLOAD_FOLDER
+
 mysql = MySQL(app)
 
 @app.route('/', methods =["GET", "POST"])
 def defaultpage():
-    return("Default landing page")
+    return render_template('index.html')
 
-
-def main():
-
-    app.run(host='localhost', port=5000)
-
+@app.route("/", methods=['POST'])
+def uploadFiles():
+    # get the uploaded file
+    uploaded_file = request.files['file']
+    if uploaded_file.filename != '':
+        file_path = os.path.join(app.config['UPLOAD_FOLDER'], uploaded_file.filename)
+        # set the file path
+        uploaded_file.save(file_path)
+        # save the file
+    return redirect(url_for('index'))
 
 def parseCSV_covidcases(filePath):
       # CVS Column Names
@@ -41,25 +51,32 @@ def parseCSV_covidcases(filePath):
              print(i,row['OBJECTID'],row['DATE'],row['Baltimore'],row['Baltimore_CITY'])
 
 def parseCSV_crime(filePath):
+    mycursor.execute("CREATE TABLE crimedata (X VARCHAR(255), Y VARCHAR(255), RowID VARCHAR(255), CrimeCode VARCHAR(255), Location VARCHAR(255), \
+                    Description VARCHAR(255), Inside_Outside VARCHAR(255), Weapon VARCHAR(255), Post VARCHAR(255), Gender VARCHAR(255), Age VARCHAR(255), \
+                    Race VARCHAR(255), Ethnicity VARCHAR(255), District VARCHAR(255), Neighborhood VARCHAR(255), Latitude VARCHAR(255), Longitude VARCHAR(255), \
+                    GeoLocation VARCHAR(255), Premise VARCHAR(255), VRIName VARCHAR(255), Total_Incidents VARCHAR(255))")
     # CVS Column Names
-      col_names = ['X','Y','RowID', 'CrimeCode', 'Location', 'Description', 'Inside_Outside', 'Weapon',
-                    'Post', 'Gender', 'Age', 'Race', 'Ethnicity', 'District', 'Neighborhood', 'Latitude',
-                    'Longitude', 'GeoLocation', 'Premise', 'VRIName', 'Total_Incidents']
-      # Use Pandas to parse the CSV file
-      csvData = pd.read_csv(filePath,names=col_names, header=None)
-      # Loop through the Rows
-      for i,row in csvData.iterrows():
-             sql = "INSERT INTO addresses (X, Y, CrimeCode, Location, Description, Inside_Outside, Weapon,\
+    col_names = ['X','Y', 'RowID', 'CrimeCode', 'Location', 'Description', 'Inside_Outside', 'Weapon',
+                'Post', 'Gender', 'Age', 'Race', 'Ethnicity', 'District', 'Neighborhood', 'Latitude',
+                'Longitude', 'GeoLocation', 'Premise', 'VRIName', 'Total_Incidents']
+    # Use Pandas to parse the CSV file
+    csvData = pd.read_csv(filePath,names=col_names, header=None)
+    csvData = csvData.where((pd.notnull(csvData)), None)
+    # Loop through the Rows
+    for i,row in csvData.iterrows():
+            sql = "INSERT INTO crimedata (X, Y, RowID, CrimeCode, Location, Description, Inside_Outside, Weapon,\
                     Post, Gender, Age, Race, Ethnicity, District, Neighborhood, Latitude, Longitude, GeoLocation,\
-                    Premise, VRIName, Total_Incidents) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
-             value = (row['X'],row['Y'],row['CrimeCode'],row['Location'], row['Description'],row['Inside_Outside'],row['Weapon'],row['Post'],
+                    Premise, VRIName, Total_Incidents) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+            value = (row['X'],row['Y'],row['RowID'],row['CrimeCode'],row['Location'], row['Description'],row['Inside_Outside'],row['Weapon'],row['Post'],
                     row['Gender'],row['Age'],row['Race'],row['Ethnicity'], row['District'],row['Neighborhood'],row['Latitude'],row['Longitude'],
                     row['GeoLocation'],row['Premise'],row['VRIName'],row['Total_Incidents'])
-             mycursor.execute(sql, value, if_exists='append')
-             mydb.commit()
-             print(i,row['X'],row['Y'],row['CrimeCode'],row['Location'], row['Description'],row['Inside_Outside'],row['Weapon'],row['Post'],
-                    row['Gender'],row['Age'],row['Race'],row['Ethnicity'], row['District'],row['Neighborhood'],row['Latitude'],row['Longitude'],
-                    row['GeoLocation'],row['Premise'],row['VRIName'],row['Total_Incidents'])
+            mycursor.execute(sql, value)
+            mydb.commit()
+
+
+def main():
+    parseCSV_crime(r"C:\Users\lukec\Desktop\Part_1_Crime_Data_.csv")
+    app.run(host='localhost', port=5000)
 
 if __name__=="__main__":
     main()
