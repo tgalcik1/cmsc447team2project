93983b61284799ab373f7c2a05d02f60f64e3694
Roberto Villanueva <98823044+RobertoV11@users.noreply.github.com>
2022-10-31 15:59:32 -0400

    Update FlaskServer.py

--- project-backend/FlaskServer.py
+++ project-backend/FlaskServer.py
@@ -17,6 +17,18 @@ def main():
     app.run(host='localhost', port=5000)
 
 
+def parseCSV(filePath):
+      # CVS Column Names
+      col_names = ['first_name','last_name','address', 'street', 'state' , 'zip']
+      # Use Pandas to parse the CSV file
+      csvData = pd.read_csv(filePath,names=col_names, header=None)
+      # Loop through the Rows
+      for i,row in csvData.iterrows():
+             sql = "INSERT INTO addresses (first_name, last_name, address, street, state, zip) VALUES (%s, %s, %s, %s, %s, %s)"
+             value = (row['first_name'],row['last_name'],row['address'],row['street'],row['state'],str(row['zip']))
+             mycursor.execute(sql, value, if_exists='append')
+             mydb.commit()
+             print(i,row['first_name'],row['last_name'],row['address'],row['street'],row['state'],row['zip'])
 
 if __name__=="__main__":
     main()
