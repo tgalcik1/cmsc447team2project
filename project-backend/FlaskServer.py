d28110ade407e3f80e73447ea7d66900aed57e23
cluke1 <cluke1@umbc.edu>
2022-11-03 17:52:03 -0400

    Fixed crime data database upload

--- project-backend/FlaskServer.py
+++ project-backend/FlaskServer.py
@@ -76,26 +76,26 @@ def parseCSV_coviddeaths(filePath):
         mydb.commit()
 
 def parseCSV_crime(filePath):
-    mycursor.execute("CREATE TABLE crimedata (X VARCHAR(255), Y VARCHAR(255), RowID VARCHAR(255), CrimeCode VARCHAR(255), Location VARCHAR(255), \
+    mycursor.execute("CREATE TABLE crimedata (X VARCHAR(255), Y VARCHAR(255), RowID VARCHAR(255), CrimeDateTime VARCHAR(255), CrimeCode VARCHAR(255), Location VARCHAR(255), \
                     Description VARCHAR(255), Inside_Outside VARCHAR(255), Weapon VARCHAR(255), Post VARCHAR(255), Gender VARCHAR(255), Age VARCHAR(255), \
                     Race VARCHAR(255), Ethnicity VARCHAR(255), District VARCHAR(255), Neighborhood VARCHAR(255), Latitude VARCHAR(255), Longitude VARCHAR(255), \
-                    GeoLocation VARCHAR(255), Premise VARCHAR(255), VRIName VARCHAR(255), Total_Incidents VARCHAR(255))")
+                    GeoLocation VARCHAR(255), Premise VARCHAR(255), VRIName VARCHAR(255), Total_Incidents VARCHAR(255), Shape VARCHAR(255))")
     # CVS Column Names
-    col_names = ['X','Y', 'RowID', 'CrimeCode', 'Location', 'Description', 'Inside_Outside', 'Weapon',
+    col_names = ['X','Y', 'RowID', 'CrimeDateTime', 'CrimeCode', 'Location', 'Description', 'Inside_Outside', 'Weapon',
                 'Post', 'Gender', 'Age', 'Race', 'Ethnicity', 'District', 'Neighborhood', 'Latitude',
-                'Longitude', 'GeoLocation', 'Premise', 'VRIName', 'Total_Incidents']
+                'Longitude', 'GeoLocation', 'Premise', 'VRIName', 'Total_Incidents', 'Shape']
     # Use Pandas to parse the CSV file
     csvData = pd.read_csv(filePath,names=col_names, header=None)
     csvData = csvData.where((pd.notnull(csvData)), None)
-    print(csvData)
+    
     # Loop through the Rows
     for i,row in csvData.iterrows():
-            sql = "INSERT INTO crimedata (X, Y, RowID, CrimeCode, Location, Description, Inside_Outside, Weapon,\
+            sql = "INSERT INTO crimedata (X, Y, RowID, CrimeDateTime, CrimeCode, Location, Description, Inside_Outside, Weapon,\
                     Post, Gender, Age, Race, Ethnicity, District, Neighborhood, Latitude, Longitude, GeoLocation,\
-                    Premise, VRIName, Total_Incidents) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
-            value = (row['X'],row['Y'],row['RowID'],row['CrimeCode'],row['Location'], row['Description'],row['Inside_Outside'],row['Weapon'],row['Post'],
+                    Premise, VRIName, Total_Incidents, Shape) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+            value = (row['X'],row['Y'],row['RowID'],row['CrimeDateTime'],row['CrimeCode'],row['Location'], row['Description'],row['Inside_Outside'],row['Weapon'],row['Post'],
                     row['Gender'],row['Age'],row['Race'],row['Ethnicity'], row['District'],row['Neighborhood'],row['Latitude'],row['Longitude'],
-                    row['GeoLocation'],row['Premise'],row['VRIName'],row['Total_Incidents'])
+                    row['GeoLocation'],row['Premise'],row['VRIName'],row['Total_Incidents'],row['Shape'])
             mycursor.execute(sql, value)
             mydb.commit()
 
@@ -104,7 +104,7 @@ def main():
    # parseCSV_crime(r"C:\Users\rober\Downloads\Part_1_Crime_Data_.csv")
     #parseCSV_covidcases(r"C:\Users\lukec\Desktop\MDCOVID19_CasesByCounty.csv")
     #parseCSV_coviddeaths(r"C:\Users\lukec\Desktop\MDCOVID19_ConfirmedDeathsByCounty.csv")
-    parseCSV_crime(r"C:\Users\rober\Downloads\Part_1_Crime_Data_.csv")
+    parseCSV_crime(r"C:\Users\lukec\Desktop\Part_1_Crime_Data_.csv")
     #parseCSV_covidcases(r"C:\Users\rober\Downloads\MDCOVID19_CasesByCounty.csv")
     app.run(host='localhost', port=5000)
 
