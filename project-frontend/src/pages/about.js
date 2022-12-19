2ee5ac57a2386f75db782def5b7d37a86dfa9033
Opeyemi Kusemiju <108900641+OpeyemiK@users.noreply.github.com>
2022-12-19 13:41:07 -0500

    About page template.

--- project-frontend/src/pages/about.js
+++ project-frontend/src/pages/about.js
@@ -12,6 +12,89 @@ const About = () => {
       }}
     >
       <h1>About page</h1>
+      <h3>Fuckers in school telling me, always in the barber shop
+"Chief Keef ain't 'bout this, Chief Keef ain't 'bout that"
+My boy a BD on fucking Lamron and them
+He, he, they say that nigga don't be putting in no work
+Shut the fuck up
+Y'all niggas ain't know shit
+All y'all motherfuckers talkin' about
+"Chief Keef ain't no hitter
+Chief Keef ain't this
+Chief Keef a fake"
+Shut the fuck up
+Y'all don't live with that nigga
+Y'all know that nigga got caught with a ratchet
+Shootin' at the police and shit
+Nigga been on probation since fuckin' I don't know when
+Motherfucker, stop fuckin' playin' him like that
+Them niggas savages out there
+If I catch another motherfucker talking sweet about Chief Keef
+I'm fucking beatin' they ass
+I'm not fucking playin' no more
+Y'all know them niggas roll with Lil Reese and them.</h3>
+
+
+      
+<h1>Our Team</h1>
+<div class="row">
+  <div class="column">
+    <div class="card">
+      <div class="container">
+        <h2>Tristan Galcik</h2>
+        <p class="title">Sprint Leader/Frontend Developer</p>
+        <p>No dick, no balls, and probably no butthole since this guy feeds on radiation</p>
+        <p>fakeemail@fake.com</p>
+      </div>
+    </div>
+  </div>
+
+  <div class="column">
+    <div class="card">
+      <div class="container">
+        <h2>Caleb Luke</h2>
+        <p class="title">Frontend Developer</p>
+        <p>Ratio + Young boy better.</p>
+        <p>fakeemail@fake.com</p>
+      </div>
+    </div>
+  </div>
+
+  <div class="column">
+    <div class="card">
+      <div class="container">
+        <h2>Roberto Villanueva</h2>
+        <p class="title">Backend Developer</p>
+        <p>W Rizz</p>
+        <p>fakeemail@fake.com</p>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="row">
+<div class="column">
+    <div class="card">
+      <div class="container">
+        <h2>Opeyemi Kusemiju</h2>
+        <p class="title">Backend Developer</p>
+        <p>Candice balls fit in yo mouf?</p>
+        <p>fakeemail@fake.com</p>
+      </div>
+    </div>
+  </div>
+
+<div class="column">
+    <div class="card">
+      <div class="container">
+        <h2>Obinna Ezejiofor</h2>
+        <p class="title">Frontend Developer</p>
+        <p>W manz</p>
+        <p>fakeemail@fake.com</p>
+      </div>
+    </div>
+  </div>
+</div>
     </div>
   );
 };
