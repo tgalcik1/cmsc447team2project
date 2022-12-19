a6e75d90202fc05e52b55fcecdd30ce7b5401e5a
tgalcik1 <tgalcik1@umbc.edu>
2022-12-19 15:47:16 -0500

    CSS for about page

--- project-frontend/src/pages/about.js
+++ project-frontend/src/pages/about.js
@@ -12,8 +12,14 @@ const About = () => {
       }}
     >
       {/* <p style="color: #ffffff"> </p> */}
-      <h1>About page</h1>
-      <h3>In production.....</h3>
+      <h1>About</h1>
+      <h3>Our product is a Baltimore City COVID-19 and Crime Data Visualizer. The main functionalities of this product include the ability to view approximately 200,000 crime cases visualized on an interactive heatmap and the ability to view approximately 200,000 COVID cases and 4,000 deaths on interactive charts and graphs. Our goal in creating these functionalities are to allow the user to explore the relationship between COVID-19 and Crime in Baltimore City. To enable the user to explore this relationship further, we have implemented various data filtering options - for example, the capability to filter by year, type of crime, district, or gender. 
+</h3>
+      <h3>Our sources for all the data are below:
+      <li><a href="https://data.baltimorecity.gov/datasets/baltimore::part-1-crime-data-/explore?location=39.310722%2C-76.584862%2C15.52&showTable=true">Crime Data</a></li>
+      <li><a href="https://coronavirus.maryland.gov/datasets/maryland::mdcovid19-casesbycounty/explore">COVID Cases Data</a></li>
+      <li><a href="https://coronavirus.maryland.gov/datasets/maryland::mdcovid19-confirmeddeathsbycounty/explore?filters=eyJCYWx0aW1vcmUiOlswLDI2MTJdLCJCYWx0aW1vcmVfQ2l0eSI6WzAsMTg1M119">COVID Deaths Data</a></li>
+      </h3>
 
 
       
@@ -24,8 +30,7 @@ const About = () => {
       <div class="container">
         <h2>Tristan Galcik</h2>
         <p class="title">Sprint Leader/Frontend Developer</p>
-        <p>its ya boy skinny penis</p>
-        <p>fakeemail@fake.com</p>
+        <p>tgalcik1@umbc.edu</p>
       </div>
     </div>
   </div>
@@ -35,8 +40,7 @@ const About = () => {
       <div class="container">
         <h2>Caleb Luke</h2>
         <p class="title">Frontend Developer</p>
-        <p>I'm the typa guy to fuck the janitor in exchange for mop water</p>
-        <p>fakeemail@fake.com</p>
+        <p>cluke1@umbc.edu</p>
       </div>
     </div>
   </div>
@@ -46,8 +50,7 @@ const About = () => {
       <div class="container">
         <h2>Roberto Villanueva</h2>
         <p class="title">Backend Developer</p>
-        <p>W Rizz</p>
-        <p>fakeemail@fake.com</p>
+        <p>robertv1@umbc.edu</p>
       </div>
     </div>
   </div>
@@ -59,8 +62,7 @@ const About = () => {
       <div class="container">
         <h2>Opeyemi Kusemiju</h2>
         <p class="title">Backend Developer</p>
-        <p>A successful man is one who earns more than his wife can spend. A successful woman is one who knows where to look for such a man.</p>
-        <p>fakeemail@fake.com</p>
+        <p>opeyemk1@umbc.edu</p>
       </div>
     </div>
   </div>
@@ -70,8 +72,7 @@ const About = () => {
       <div class="container">
         <h2>Obinna Ezejiofor</h2>
         <p class="title">Frontend Developer</p>
-        <p>Can cars stop at a bus stop?</p>
-        <p>fakeemail@fake.com</p>
+        <p>obinnae1@umbc.edu</p>
       </div>
     </div>
   </div>
